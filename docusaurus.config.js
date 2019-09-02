1d7b10fdc66a08b3df0eb02cc3013598de1bd9bd
acetousk <acetousk@gmail.com>
2019-09-01 22:09:30 -0600

    :a:utoLib Docs! Initial Commit

---
+++ docusaurus.config.js
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  title: 'AutoLib Docs',
+  tagline: 'AutoLib is a Pros C++ Library for Advanced Vex Autonomous Programming',
+  url: 'https://autolib-docs.netlify.com',
+  baseUrl: '/',
+  favicon: 'img/favicon.png',
+  organizationName: 'team914', // Usually your GitHub org/user name.
+  projectName: 'autolib-docs', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'AutoLib',
+      logo: {
+        alt: 'AutoLib Logo',
+        src: 'img/logo.svg',
+      },
+      links: [
+        {to: 'docs/doc1', label: 'Docs', position: 'left'},
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/team914/autolib',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Docs',
+              to: 'docs/doc1',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+          ],
+        },
+      ],
+      logo: {
+        alt: 'AutoLib Open Source Logo',
+        src: '',
+      },
+      copyright: `Copyright © ${new Date().getFullYear()} AutoLib. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
